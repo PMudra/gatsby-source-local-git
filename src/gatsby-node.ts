6ebb175d651684e1b5ffe33797e61f47a37a06e1
Philipp Mudra <philipp.mudra@web.de>
2019-11-12 22:34:53 +0000

    Even more rework

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -1,103 +1,31 @@
-import { SourceNodesArgs, GatsbyNode, NodeInput } from "gatsby"
+import { SourceNodesArgs, GatsbyNode } from "gatsby"
 import { getCommits, Commit, Author } from "./git"
+import { createNodeFactory } from "./gatsby-node-helper"
 
-interface Node {
-  id: string
-}
-
-interface PrepareNodeHelpers {
-  createContentDigest: SourceNodesArgs["createContentDigest"]
-  createNodeId: SourceNodesArgs["createNodeId"]
-}
-
-const prepareNode = <T extends Node>(
-  node: T,
-  type: string,
-  { createNodeId, createContentDigest }: PrepareNodeHelpers
-) => ({
-  ...node,
-  id: createNodeId(node.id),
-  internal: {
-    type,
-    contentDigest: createContentDigest(node),
-  },
-})
-
-interface CreateNodeFactoryHelpers extends PrepareNodeHelpers {
-  createNode: SourceNodesArgs["actions"]["createNode"]
-}
-
-const identity = (value: any) => value
-
-const createNodeFactory = <T extends Node>(
-  type: string,
-  { createNode, ...helpers }: CreateNodeFactoryHelpers,
-  preprocessNode: (node: T, helpers: PrepareNodeHelpers) => T = identity
-) => (node: T) =>
-  createNode(prepareNode(preprocessNode(node, helpers), type, helpers))
-
-const sourceNodes: GatsbyNode["sourceNodes"] = async ({
-  createContentDigest,
-  createNodeId,
-  actions: { createNode },
-}: SourceNodesArgs) => {
-  const helpers: CreateNodeFactoryHelpers = {
-    createNode,
-    createNodeId,
-    createContentDigest,
+const sourceNodes: GatsbyNode["sourceNodes"] = async (
+  sourceNodesArgs: SourceNodesArgs
+) => {
+  const helpers = {
+    ...sourceNodesArgs,
+    createNode: sourceNodesArgs.actions.createNode,
   }
 
-  const createCommitNode = createNodeFactory<Commit>("GitCommit", helpers)
+  const createCommitNode = createNodeFactory<Commit>(
+    "GitCommit",
+    helpers,
+    ({ author, ...commit }, { createNodeId }) => ({
+      ...commit,
+      author___NODE: createNodeId(author.id),
+    })
+  )
+  const createAuthorNode = createNodeFactory<Author>("GitAuthor", helpers)
 
   const commits = await getCommits()
 
-  commits.forEach(commit => createCommitNode(commit))
-
-  // const authorsByAuthorId = new Map<
-  //   String,
-  //   { name: String; email: String; commits: Array<String> }
-  // >()
-
-  // commits.forEach(commit => {
-  //   const {
-  //     author: { id: authorId, name, email },
-  //     ...filteredCommit
-  //   } = commit
-
-  // const author = authorsByAuthorId.get(authorId) || {
-  //   name,
-  //   email,
-  //   commits: [commit.hash],
-  // }
-
-  // authorsByAuthorId.set(authorId, {
-  //   ...author,
-  //   commits: [...author.commits, commit.hash],
-  // })
-
-  //   createNode({
-  //     ...filteredCommit,
-  //     author___NODE: createNodeId(authorId),
-  //     id: createNodeId(commit.hash),
-  //     internal: {
-  //       type: "GitCommit",
-  //       contentDigest: createContentDigest(commit),
-  //     },
-  //   })
-  // })
-
-  // authorsByAuthorId.forEach((author, id) => {
-  //   const { commits, ...filteredAuthor } = author
-  //   createNode({
-  //     ...filteredAuthor,
-  //     commits___NODE: commits.map(hash => createNodeId(hash)),
-  //     id: createNodeId(id),
-  //     internal: {
-  //       type: "GitAuthor",
-  //       contentDigest: createContentDigest(author),
-  //     },
-  //   })
-  // })
+  commits.forEach(commit => {
+    createCommitNode(commit)
+    createAuthorNode(commit.author)
+  })
 }
 
 export { sourceNodes }
