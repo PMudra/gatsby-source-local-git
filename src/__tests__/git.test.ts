5ac905e41fd34b186ee86b08539407f6114336de
Philipp Mudra <philipp.mudra@web.de>
2019-12-08 13:25:59 +0000

    Improve git access tests #21

--- src/__tests__/git.test.ts
+++ src/__tests__/git.test.ts
@@ -5,9 +5,19 @@ test("First commit should match snapshot", async () => {
   expect(commits.pop()).toMatchSnapshot()
 })
 
+test("Exactly one commit should be latest", async () => {
+  const commits = await getCommits()
+  expect(commits.filter(commit => commit.latest)).toHaveLength(1)
+})
+
 test("First tag should match snapshot", async () => {
   const [firstTag] = await getTags()
   expect(firstTag).toMatchSnapshot()
+})
+
+test("Exactly one tag should be latest", async () => {
+  const tags = await getTags()
+  expect(tags.filter(tag => tag.latest)).toHaveLength(1)
 })
 
 test("Exactly one branch should be current", async () => {
