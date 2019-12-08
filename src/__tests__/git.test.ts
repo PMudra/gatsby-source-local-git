3bd45cd193a19c2b890b5453b05c21fee40fe465
Philipp Mudra <philipp.mudra@web.de>
2019-12-08 01:31:05 +0000

    Add tests for git access

--- src/__tests__/git.test.ts
+++ src/__tests__/git.test.ts
@@ -1,1 +1,16 @@
-test.todo("write tests")
+import { getBranches, getCommits, getTags } from "../git"
+
+test("First commit should match snapshot", async () => {
+  const commits = await getCommits()
+  expect(commits.pop()).toMatchSnapshot()
+})
+
+test("First tag should match snapshot", async () => {
+  const [firstTag] = await getTags()
+  expect(firstTag).toMatchSnapshot()
+})
+
+test("Exactly one branch should be current", async () => {
+  const branches = await getBranches()
+  expect(branches.filter(branch => branch.current)).toHaveLength(1)
+})
