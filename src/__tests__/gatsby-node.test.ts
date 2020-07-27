6102d49ee2ed54e837cbd5611729ffb2ae070533
Philipp Mudra <philipp.mudra@web.de>
2020-07-27 22:18:23 +0200

    Fix tests

--- src/__tests__/gatsby-node.test.ts
+++ src/__tests__/gatsby-node.test.ts
@@ -4,7 +4,10 @@ import { SourceNodesArgs } from "gatsby"
 test("Calling sourceNodes should call createNode method", async () => {
   const createNode = jest.fn()
   await sourceNodes({
-    actions: { createNode: createNode as Function },
+    actions: {
+      createNode: createNode as Function,
+      createTypes: new Function(),
+    },
     createNodeId: new Function(),
     createContentDigest: new Function(),
   } as SourceNodesArgs)
